09ead0fdabbc02dbe81ddb4da1f18f07a537d358
balaiyer <bala.iyer1973@gmail.com>
2012-10-10 11:10:22 +0100

    Fixed pagination of donations by supplying correct param name for api call

--- js/justgiving.js
+++ js/justgiving.js
@@ -37,7 +37,7 @@ JustGivingWinJS.ImageUrlFromImage = function(url, imageType) {
 JustGivingWinJS.DonationForPage = function (pageShortUrl, pageNumber) {
     var url = 'https://api.justgiving.com/' + JustGivingWinJS.ApiKey + '/v1/fundraising/pages/' + pageShortUrl + '/donations?pageSize=5';
     if (pageNumber) {
-        url = url + '&pageNumber=' + pageNumber;
+        url = url + '&pageNum=' + pageNumber;
     }
     return WinJS.xhr({ url: url, headers: { 'content-type': 'application/json' } });
 };
